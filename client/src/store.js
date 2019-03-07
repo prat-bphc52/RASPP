b77c4fb1089ee013506eadc63a054a0f96162784
prat-bphc52 <hp.agarwal1@gmail.com>
2019-03-07 19:30:53 +0530

    Fix for all browsers

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,19 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+// import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
 const initialState = {};
 
 const middleware = [thunk];
-console.log("test2")
 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
   )
 );
 
 export default store;
+// ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
